33b29e9507330f31a79ab656ad887713eeb121ec
MasamichiWatabe <wangtang1206@gmail.com>
2018-10-12 14:21:16 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/masamichi/watabe/javalog/Movable.java
@@ -0,0 +1,5 @@
+package jp.techacademy.masamichi.watabe.javalog;
+
+interface Movable {
+    public void move();
+}
---
+++ app/src/main/java/jp/techacademy/masamichi/watabe/javalog/Thinkable.java
@@ -0,0 +1,5 @@
+package jp.techacademy.masamichi.watabe.javalog;
+
+interface Thinkable {
+    public void think();
+}
